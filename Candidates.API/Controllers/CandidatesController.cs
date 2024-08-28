45bafc40b396370ddf542460432372c94b6ae439
Owais-AlShishani <98020718+Owais-AlShishani@users.noreply.github.com>
2024-08-28 19:37:11 +0300

    Added Candidates Controller

---
+++ Candidates.API/Controllers/CandidatesController.cs
@@ -0,0 +1,43 @@
+﻿using Candidates.API.Data;
+using Candidates.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Candidates.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CandidatesController(ApplicationDbContext dbContext) : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Create(Candidate candidate)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var existingCandidate = await dbContext.Candidates.SingleOrDefaultAsync(c => c.Email == candidate.Email);
+            if (existingCandidate is null)
+            {
+                // Creation
+                dbContext.Add(candidate);
+                int affected = await dbContext.SaveChangesAsync();
+                return Created();
+            }
+
+            else
+            {
+                existingCandidate.FirstName = candidate.FirstName;
+                existingCandidate.LastName = candidate.LastName;
+                existingCandidate.PhoneNumber = candidate.PhoneNumber;
+                existingCandidate.CallTimeInterval = candidate.CallTimeInterval;
+                existingCandidate.LinkedInProfileUrl = candidate.LinkedInProfileUrl;
+                existingCandidate.GitHubProfileUrl = candidate.GitHubProfileUrl;
+                existingCandidate.Comments = candidate.Comments;
+                int afftected =await dbContext.SaveChangesAsync();
+                return Ok(candidate);
+            }
+        }
+    }
+}
